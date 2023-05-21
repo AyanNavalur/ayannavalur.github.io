ed19fee469eb46a99f9bdd01ed47a1fd3ecc5bbc
Ayan Navalur <ayannavalur@gmail.com>
2023-05-21 12:19:26 -0400

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+import Logo from '../assets/logo.png'
+
+const NavBar = () => {
+    const [nav, setNav] = useState(false);
+    const handleClick = () => {
+        setNav(!nav);
+    }
+
+    return (
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+            <div>
+                <img src={Logo} alt='Ayan Navalur' style={{width: '50px'}} />
+            </div>
+            {/* Menu */}
+            <ul className='hidden md:flex'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+
+            {/* Hamburger */}
+            <div onClick={handleClick} className='md:hidden z-10'>
+                {!nav ? <FaBars /> : <FaTimes />}
+            </div>
+
+            {/* Mobile Menu */}
+            <ul className= {!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+                <li className='py-6 text-4xl'>Home</li>
+                <li className='py-6 text-4xl'>About</li>
+                <li className='py-6 text-4xl'>Skills</li>
+                <li className='py-6 text-4xl'>Projects</li>
+                <li className='py-6 text-4xl'>Contact</li>
+            </ul>
+
+            {/* Social Icons */}
+            <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+                <ul >
+                    <li className='w-[130px] h-[60px] flex justify-between items-center ml-[-78px] hover:ml-[-5px] duration-300 bg-blue-600'>
+                        <a className='flex justify-between items-center w-full text-gray-300' href='/'>LinkedIn <FaLinkedin size={30}/></a>
+                    </li>
+                    <li className='w-[130px] h-[60px] flex justify-between items-center ml-[-78px] hover:ml-[-5px] duration-300 bg-[#333333]'>
+                        <a className='flex justify-between items-center w-full text-gray-300' href='/'>GitHub <FaGithub size={30}/></a>
+                    </li>
+                    <li className='w-[130px] h-[60px] flex justify-between items-center ml-[-78px] hover:ml-[-5px] duration-300 bg-[#6fc2b0]'>
+                        <a className='flex justify-between items-center w-full text-gray-300' href='/'>Email <HiOutlineMail size={30}/></a>
+                    </li>
+                    <li className='w-[130px] h-[60px] flex justify-between items-center ml-[-78px] hover:ml-[-5px] duration-300 bg-[#565f69]'>
+                        <a className='flex justify-between items-center w-full text-gray-300' href='/'>Resume <BsFillPersonLinesFill size={30}/></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
---
+++ src/components/Skills.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import HTML from '../assets/html.png'
+import CSS from '../assets/css.png'
+import JavaScript from '../assets/javascript.png'
+import AWS from '../assets/aws.png'
+
+const Skills = () => {
+  return (
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+        {/* Container */}
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
+                <p className='py-4'>// These are the technologies I'm familiar with</p>
+            </div>
+
+            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto my-2' src={HTML} alt='HTML' />
+                    <p className='my-4'>HTML</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto my-2' src={CSS} alt='CSS' />
+                    <p className='my-4'>CSS</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto my-2' src={JavaScript} alt='JavaScript' />
+                    <p className='my-4'>JavaScript</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto my-2' src={AWS} alt='AWS' />
+                    <p className='my-4'>AWS</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Skills
