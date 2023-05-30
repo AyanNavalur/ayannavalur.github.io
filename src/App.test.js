459949e05b43b2cd806e1357386a135019843fd6
Ayan Navalur <ayannavalur@gmail.com>
2023-05-30 13:35:31 -0400

    test files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Ayan Navalur/i);
   expect(linkElement).toBeInTheDocument();
 });
